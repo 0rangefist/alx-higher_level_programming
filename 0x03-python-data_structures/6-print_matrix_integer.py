77648566de542f89e737a92257f567dd0aaa7ad0
Thomas <collinskthomas@gmail.com>
2023-05-12 10:59:52 +0000

    Add py script (6-print_matrix_integer.py)

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, elem in enumerate(row):
+            print("{:d}".format(elem), end="" if i == len(row) - 1 else " ")
+        # newline after each row
+        print()
