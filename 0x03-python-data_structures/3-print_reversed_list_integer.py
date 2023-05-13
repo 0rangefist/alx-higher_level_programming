7a9952be42f711223b95ddf2f758717f1f9921e9
Thomas <collinskthomas@gmail.com>
2023-05-13 10:04:34 +0000

    add edge case to py script (3-print_reversed_list_integer.py)

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    # handle when input is empty [] or None
+    if not my_list:
+        return
     for elem in reversed(my_list):
         print("{:d}".format(elem))
