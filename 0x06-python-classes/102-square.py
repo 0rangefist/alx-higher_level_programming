e2f725b0ba7b186cc59d5f01174feb5ecd060293
Thomas <collinskthomas@gmail.com>
2023-05-31 10:58:04 +0000

    Add py script that implements oop logical ops (102-square.py)

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square with a size
+"""
+
+
+class Square:
+    """
+    Square class with a private size attribute
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the Square instance
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the current square area
+        """
+        return self.__size * self.__size
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
