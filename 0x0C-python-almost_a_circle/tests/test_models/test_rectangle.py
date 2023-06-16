19d0da11d4483a24536377ece3366bcf20b38022
Thomas <collinskthomas@gmail.com>
2023-06-16 12:48:31 +0000

    Update Rectangle class with validation & update its unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,14 +11,14 @@ class TestRectangleClass(unittest.TestCase):
     """
     Unit tests for the Rectangle class
     """
+
     def setUp(self):
         self.rect1 = Rectangle(2, 10)
         self.rect2 = Rectangle(10, 2, 1, 1, 12)
         self.rect3 = Rectangle(12, 3)
 
     def test_ids(self):
-
-        self.assertNotEqual(self.rect1.id, self.rect3.id)
+        self.assertNotEqual(self.rect1.id, self.rect2.id)
         self.assertEqual(self.rect2.id, 12)
 
     def test_getters(self):
@@ -41,3 +41,41 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(self.rect2.height, 9)
         self.assertEqual(self.rect2.x, 2)
         self.assertEqual(self.rect2.y, 3)
+
+    def test_width_type_validation(self):
+        bad_input_types = [None, True, False, 5.5, [1, 2, 3], {"a": 1, "b": 2}]
+        for bad_input_type in bad_input_types:
+            with self.assertRaisesRegex(TypeError,
+                                        "width must be an integer"):
+                self.rect1.width = bad_input_type
+
+    def test_width_value_validation(self):
+        bad_input_values = [0, -1, -100, -100000]  # 0 and negatives
+        for bad_input_value in bad_input_values:
+            with self.assertRaisesRegex(ValueError, "width must be > 0"):
+                self.rect1.width = bad_input_value
+
+    def test_height_type_validation(self):
+        bad_input_types = [None, True, False, 5.5, [1, 2, 3], {"a": 1, "b": 2}]
+        for bad_input_type in bad_input_types:
+            with self.assertRaisesRegex(TypeError,
+                                        "height must be an integer"):
+                self.rect1.height = bad_input_type
+
+    def test_height_value_validation(self):
+        bad_input_values = [0, -1, -100, -100000]  # 0 and negatives
+        for bad_input_value in bad_input_values:
+            with self.assertRaisesRegex(ValueError, "height must be > 0"):
+                self.rect1.height = bad_input_value
+
+    def test_x_value_validation(self):
+        bad_input_values = [-1, -100, -100000]  # negatives
+        for bad_input_value in bad_input_values:
+            with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+                self.rect1.x = bad_input_value
+
+    def test_y_value_validation(self):
+        bad_input_values = [-1, -100, -100000]  # negatives
+        for bad_input_value in bad_input_values:
+            with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+                self.rect1.y = bad_input_value
