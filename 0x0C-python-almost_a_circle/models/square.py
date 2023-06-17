9cbdbdbd7ccbf329485b7168c7b4e2e007dc5252
Thomas <collinskthomas@gmail.com>
2023-06-17 09:43:10 +0000

    Create Square class from Rectangle with __init__ & overidden __str__ & update unittest

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class which inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    A square class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return (
+            f"[Square] ({self.id}) {self.x}/{self.y} - "
+            f"{self.width}"
+        )
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Unittest for Square class
+"""
+import unittest
+from models.square import Square
+
+
+class TestSquareClass(unittest.TestCase):
+    """
+    Unit tests for the Square class (which inherits from Rectangle)
+    """
+    def test_square_init(self):
+        # init with only size & default args
+        square = Square(5)
+        self.assertEqual(square.width, 5)
+        self.assertEqual(square.height, 5)
+        self.assertEqual(square.x, 0)
+        self.assertEqual(square.y, 0)
+        self.assertIsNotNone(square.id)
+
+        # init with all args
+        square = Square(6, 1, 3, 15)
+        self.assertEqual(square.width, 6)
+        self.assertEqual(square.height, 6)
+        self.assertEqual(square.x, 1)
+        self.assertEqual(square.y, 3)
+        self.assertEqual(square.id, 15)
+
+    def test_square_str(self):
+        square = Square(4, 1, 2, 10)
+        square_str = str(square)
+        self.assertEqual(square_str, "[Square] (10) 1/2 - 4")
