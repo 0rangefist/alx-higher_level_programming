73f6de0fbf5d86e0699e1ba4950cd18d8a44beec
Thomas <collinskthomas@gmail.com>
2023-06-17 12:28:00 +0000

    Update Base class with to_json_string method & update unittest

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This module defines a Base class
 """
+import json
 
 
 class Base:
@@ -16,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of a list of dictionaries
+        """
+        if not list_dictionaries or list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,13 +12,39 @@ class TestBaseClass(unittest.TestCase):
     Unit tests for the Base class
     """
 
-    def test_init_without_arg(self):
+    def test_base_init_without_arg(self):
         # IDs should be unique to every instance
         b1 = Base()
         b2 = Base()
         self.assertNotEqual(b1.id, b2.id)
 
-    def test_init_with_arg(self):
+    def test_base_init_with_arg(self):
         # ID argument can be used for initialization
         b1 = Base(12)
         self.assertEqual(b1.id, 12)
+
+    def test_base_to_json_string(self):
+        # Test with empty list
+        json_str = Base.to_json_string([])
+        self.assertEqual(json_str, "[]")
+
+        # Test with None
+        json_str = Base.to_json_string(None)
+        self.assertEqual(json_str, "[]")
+
+        # Test with list of dictionaries
+        dicts = [
+            {"id": 1, "size": 5},
+            {"id": 2, "size": 3}
+        ]
+        expected_json_str = '[{"id": 1, "size": 5}, {"id": 2, "size": 3}]'
+        json_str = Base.to_json_string(dicts)
+        self.assertEqual(json_str, expected_json_str)
+
+        # Test with no args
+        with self.assertRaises(TypeError):
+            Base.to_json_string()
+
+        # Test with more than one arg
+        with self.assertRaises(TypeError):
+            Base.to_json_string([], 1)
