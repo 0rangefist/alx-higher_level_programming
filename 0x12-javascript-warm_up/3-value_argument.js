65fe3dffaed5ebb1de87a0383f4737c1486e273c
Thomas <collinskthomas@gmail.com>
2023-08-14 14:06:54 +0000

    Add js script that prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/* if the first arg exists  */
+if (process.argv[2]) {
+  /* print it to the console */
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
+}
