347cf4438bd81458aa9a710670f268ee432e9c43
Thomas Collins <collinskthomas@gmail.com>
2023-08-18 17:55:34 +0000

    Add py script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where
+name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    #  Connect to the MySQL server
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+
+    # Start cursor
+    cur = conn.cursor()
+
+    # Query
+    query = "SELECT cities.id, cities.name, states.name \
+            FROM cities \
+            INNER JOIN states \
+            ON cities.state_id = states.id \
+            ORDER BY cities.id ASC"
+
+    # Perform query (substituting in parameter)
+    cur.execute(query)
+
+    # Get the rows of the query
+    query_rows = cur.fetchall()
+
+    # Print query results
+    for row in query_rows:
+        print(row)
+
+    # Close the cursor & db connection
+    cur.close()
+    conn.close()
